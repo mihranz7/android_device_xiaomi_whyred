a1486d708253aa461d9efc35f0df8cb5fdfb9727
Offain <fabian.leutenegger@bluewin.ch>
2019-01-05 16:40:29 +0100

    whyred: Revert back to 1.0 power HAL
    Change-Id: I397346e3951e34b6c272fc7dee0b550508aee3b9
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -316,7 +316,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
