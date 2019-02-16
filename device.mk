921b2c72358d363c5d3dff91db86b28d700088ba
Parth Bhatia <parthbhatia98@gmail.com>
2019-02-16 21:19:57 +0530

    whyred: inherit perf blobs from vendor/qcom/common
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -312,6 +312,9 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Performance
+-include vendor/qcom/common/qti-vendor.mk
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
